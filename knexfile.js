4373e58d1b7115c021427a39ebe43c717fb2f609
WillMR94 <108240544+WillMR94@users.noreply.github.com>
2023-06-16 22:34:26 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname,"src","database","database.db")
+      filename: path.resolve(__dirname,"SRC","database","database.db")
     },
     pool: {
       afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
     },
     migrations:{
-      directory: path.resolve(__dirname,"src","database","knex","migration")
+      directory: path.resolve(__dirname,"SRC","database","knex","migration")
     },
     useNullAsDefault: true
   }
